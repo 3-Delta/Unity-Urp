6c631c0df125bc8a75fb85cd801dcc3d2c32b5dd
kaclok <kaclok@users.noreply.github.com>
2021-02-28 22:52:03 +0800

    注释

--- urp/custom-srp-01-custom-render-pipeline/Assets/Custom RP/Runtime/CameraRenderer.cs
+++ urp/custom-srp-01-custom-render-pipeline/Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -27,6 +27,10 @@ public partial class CameraRenderer {
 			return;
 		}
 
+		// 为什么有的是context.xxx, 而有的是buffer.xxx, 是因为pipeline中，固定的集中操作可以直接contex.xxx
+		// 而另外一些操作需要借助cmdbuffer执行，cmdbuffer相当于一个打手
+		// contex.submit之后contex才能真正生效，而contex。executebuffer之后，会将cmdbuffer的指令复制到contex中，而不是剪切
+		// 最终通知gpu渲染肯定是submit。
 		Setup();
 		DrawVisibleGeometry();
 		DrawUnsupportedShaders();
@@ -36,6 +40,7 @@ public partial class CameraRenderer {
 
 	bool Cull () {
 		if (camera.TryGetCullingParameters(out ScriptableCullingParameters p)) {
+			// cpu进行视锥裁减，layer裁减等操作
 			cullingResults = context.Cull(ref p);
 			return true;
 		}
@@ -43,6 +48,7 @@ public partial class CameraRenderer {
 	}
 
 	void Setup () {
+		// 传递矩阵等信息给gpu
 		context.SetupCameraProperties(camera);
 		CameraClearFlags flags = camera.clearFlags;
 		buffer.ClearRenderTarget(
@@ -68,6 +74,7 @@ public partial class CameraRenderer {
 
 	void DrawVisibleGeometry () {
 		var sortingSettings = new SortingSettings(camera) {
+			// cpu进行对象排序
 			criteria = SortingCriteria.CommonOpaque
 		};
 		var drawingSettings = new DrawingSettings(
@@ -75,16 +82,19 @@ public partial class CameraRenderer {
 		);
 		var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
 
+		// 渲染不透明物体
 		context.DrawRenderers(
 			cullingResults, ref drawingSettings, ref filteringSettings
 		);
 
+		// 渲染skybox
 		context.DrawSkybox(camera);
 
 		sortingSettings.criteria = SortingCriteria.CommonTransparent;
 		drawingSettings.sortingSettings = sortingSettings;
 		filteringSettings.renderQueueRange = RenderQueueRange.transparent;
 
+		// 渲染半透明物体
 		context.DrawRenderers(
 			cullingResults, ref drawingSettings, ref filteringSettings
 		);
